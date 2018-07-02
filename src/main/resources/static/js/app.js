da98546fa4f7184cad07d09201e23a08b6deaff1
Kate <e.genova@live.co.uk>
2018-07-02 15:29:29 +0100

    navbar boostrapped

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -19,20 +19,31 @@ const Navbar = React.createClass({
 
     render: function () {
         return (
-            <div className="navbar">
-                <a href="#" onClick={this.Dashboard}>AccountsApp</a>
-                <a href="#" onClick={this.Dashboard}>Dashboard</a>
-            <div className="dropdown">
-                    <button className="dropbtn" type="button" data-toggle="collapse" data-target="#dropdown-content" aria-controls="dropdown-content" aria-expanded="false">Accounts
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-                    <div className="dropdown-content">
-                        <a href="#" >View Accounts</a>
-                        <a href="#" onClick={this.AddAccounts}>Add Account</a>
-                        <a href="#">Delete Account</a>
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#" onClick={this.Dashboard}>AccountsApp</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#" onClick={this.Dashboard}>Dashboard</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Accounts
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="#">View Accounts</a>
+                            <div className="dropdown-divider">?</div>
+                            <a className="dropdown-item" href="#" onClick={this.AddAccounts}>Add Account</a>
+                            <a className="dropdown-item" href="#">Delete Account</a>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            </div>
+        </nav>
         );
     }
 });
